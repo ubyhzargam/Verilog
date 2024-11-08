52b50a8f284620a22beab4fc613d0b98bf03d88a
Uby H <136114596+ubyhzargam@users.noreply.github.com>
2024-11-08 23:14:40 +0530

    Create simpleckt1.v

---
+++ simpleckt1.v
@@ -0,0 +1,5 @@
+// Circuit B can be described by the following simulation waveform: https://hdlbits.01xz.net/wiki/Mt2015_q4b
+
+module top_module ( input x, input y, output z );
+    assign z=~(x^y);
+endmodule
