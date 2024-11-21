295110a43f1a2da9bb606cd721d10463cc087e52
Uby H <136114596+ubyhzargam@users.noreply.github.com>
2024-11-21 20:03:14 +0530

    Update fsm101.v

--- fsm101.v
+++ fsm101.v
@@ -5,14 +5,10 @@
 
 module fsm(input clk, input rst, input x, output reg y);
 
-   parameter [1:0] S0=2'b00,S1=2'b01,S2=2'b10,S3=2'b11;
+parameter S0=2'b00,S1=2'b01,S2=2'b10,S3=2'b11;
 
 reg [1:0] ps,ns;
 
-initial
-begin
-ps<=S0;ns<=S0;
-end
 
 always@(posedge clk )
 begin
@@ -26,18 +22,21 @@ end
 
 always@(ps or x )
 begin
+ns<=S0;
 case(ps)
-S0:if(x==0)begin y<=0;ns<=S0;end
-   else begin y<=0;ns<=S1;end
-S1:if(x==0)begin y<=0;ns<=S2;end
-   else begin y<=0;ns<=S1;end
-S2:if(x==0)begin y<=0;ns<=S0;end
-   else begin y<=1;ns<=S3;end
-S3:if(x==0)begin ns<=S2;y<=1;end
-   else begin ns<=S1;y<=1;end
+S0:begin if(x==0)begin y<=0;ns<=S0;end
+   else begin y<=0;ns<=S1;end end
+S1:begin if(x==0)begin y<=0;ns<=S2;end
+   else begin y<=0;ns<=S1;end end
+S2:begin if(x==0)begin y<=0;ns<=S0;end
+   else begin y<=1;ns<=S3;end end
+S3:begin if(x==0)begin ns<=S2;y<=1;end
+   else begin ns<=S1;y<=1;end end
 default:begin ns<=S0;y<=0;end
 endcase
 end
+
+
 endmodule
 
 // Testbench
@@ -47,25 +46,26 @@ reg clk, rst, x;
 wire y;
 fsm f0(clk,rst,x,y);
 
-always begin #2 clk = ~clk; end
+always begin #16 clk = ~clk; end
 
 initial begin
 clk=1'b0;
 x=1'b0;
-#10 rst=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b1; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 x=1'b0; $strobe($time, " rst = %b , x = %b , ",rst,x,y);
-#10 $finish;
+rst=1'b1;
+#30 rst=1'b0; 
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b1; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 x=1'b0; #1$strobe($time, " rst = %b , x = %b , ",rst,x,y);
+#30 $finish;
 end
 endmodule
